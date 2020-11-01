5c7587a91718273d0e11ce13cf04d4d2eb690f91
isapoff <iskender.saparov.is@gmail.com>
2020-11-01 17:41:52 +0600

    initial commit

---
+++ lecture.py
@@ -0,0 +1,35 @@
+# int = 2
+# print(int)
+
+# num1 = 1
+# print(num1)
+# number = input("Введите целое число:")
+# print(number)
+# print(type(num1))
+# print(type(number))
+
+
+# print(2 * 5**2 / 10 + 8)
+
+# a = 20
+# b = 3
+# print(a / b)
+# print(a // b)
+
+# print(a > b)
+
+# number = 25
+# number_2 = 5
+# print(number)
+
+# number = number - number_2
+# number -= number_2
+# number /=number_2
+# print(number)
+
+
+
+
+
+
+
