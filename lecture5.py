60fd95678c630c272b585b104892ecaf032b9219
isapoff <iskender.saparov.is@gmail.com>
2020-11-01 17:49:23 +0600

    initial commit

---
+++ lecture5.py
@@ -0,0 +1,94 @@
+# age = int(input)
+# name = "Bakyt"
+# name = input("Enter your name:")
+# surname = "Kokoev"
+# full_name = name + " " + surname
+# print(full_name)
+# times = int(input("How many time you want to print your name:"))
+
+# result = times * name
+# print(result)
+
+# print(word[3])
+# word[x:y:z]
+# print(word[1:3])
+
+# word = "abcdefgklmnopk"
+# new_word = word[-13::-2]
+# print(new_word)
+
+# word = "abcd'efg\"hk'lmn"
+
+
+# word = "nurbek\\mentor"
+# word = '''nurbek  
+
+
+# mentor'''
+
+
+# word = "\172"
+# print(word)
+
+# a = "hello"
+# print(a[-2:-1])
+
+# s = "hello world!"
+# s = s.count('l')
+# s = s.index('o')
+# s = s.replace('o','2358')
+# s = s.replace('l','')
+# s = s.replace('l','', 1)
+# s = 'kjfhgfhj'
+# s = s.isalpha()
+# s = '123'
+# s = s.isdigit()
+ 
+# b = '258'
+# b = b.rjust(7, '0')
+# b = b.ljust(10, '-')
+
+
+# w = 'ivan ivanov ivanovich'
+# # w = w.split()
+# len(w.split())
+# w = w.split('n')
+
+# t = '43,34243,3,4536,5'.split(',')
+# t = '='.join(t)
+# print(t)
+
+print('hello ' * 10 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
